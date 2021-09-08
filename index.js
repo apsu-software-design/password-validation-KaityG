d4f78dbb2397d52ff9cc7c7ba191e7b3a36e7fad
Kaitlyn <khardin9@my.apsu.edu>
2021-09-07 19:50:33 -0500

    modify index.js
    Add code to the index.js to check that a user's password is valid.

--- index.js
+++ index.js
@@ -1,3 +1,44 @@
-const validatePassword = require('./passwordvalidation'); //import the function
+const validatePassword = require('./passwordvalidation.js'); //import the function
 
 // Your code goes here!
+function getPassword()
+{
+    //Get the readline module (got code from online documentation)
+    var readline = require('readline');
+    
+    var rl = readline.createInterface(process.stdin, process.stdout);
+
+    rl.question('Enter your password:', (password) => 
+    {   
+        //Base case: quit the program
+        if (password == "quit" || password == "Quit")
+        {
+            alert("Thank you! Goodbye :)");
+            process.exit(1);
+        }
+        //check validity of user password
+        else
+        {
+            //Notify user that password is correct
+            if(validatePassword)
+            {
+                alert("Your password is valid.")
+            }
+
+            //Notify user that the password is incorrect
+            else
+            {
+                alert("Your password is invalid. Try again.")
+            }
+
+        }
+
+        //Close readline
+        rl.close();
+
+    });
+    //Recursion: call the function repeatedly
+    getPassword();
+
+    
+}
